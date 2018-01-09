ab2d8f5054579e553ec5744acab983b6354048a3
veerasuthan veerakesan <svel493@gmail.com>
2018-01-09 09:49:50 +0530

    Final commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -46,6 +46,35 @@ router.post('/userRegistration', (req, res) => {
   });
 });
 
+router.get('/editProfile', verifytoken,(req, res) => {
+
+  var userdata=req.query.data || req.body.data;
+  userdata = JSON.parse(userdata);
+  
+  var isAdmin = userdata.isAdmin !== undefined ? userdata.isAdmin :0;
+  var user_id = userdata.user_id;
+
+ 
+    var data = {
+      username: `${userdata.username}`,
+      isAdmin: isAdmin,
+      email: `${userdata.email}@knowledgeq.com`,
+      empID: userdata.empID,
+      date: new Date(new Date().toUTCString()),
+      notify: userdata.notify,
+      interest: userdata.interest
+    }
+
+    con.query("UPDATE users SET ? where user_id = ?", [data,user_id], (err, result) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.status(200).json({message:'Profile updated successfully..'});
+      }
+    });
+ 
+});
+
 router.post('/login', (req, res) => {
   var data=req.query.data || req.body.data;
   data = JSON.parse(data);
@@ -92,14 +121,21 @@ router.post('/login', (req, res) => {
 
 
 router.get('/changePassword', verifytoken, (req, res) => {
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  var token = req.body.token || req.query.token || req.headers['x-access-token'] || req.headers['authorization'];
   var data=req.query.data;
   data = JSON.parse(data);
   var userData = {
     username:data.username,
     old_password:data.old_password,
-    new_password:data.new_password
+    new_password:data.new_password,
+	confirm_password:data.confirm_password
   }
+  
+    token=token.replace("Bearer","");
+	token=token.trim();
+	console.log("@@@@@@@@@@",token);
+  
+  if(userData.confirm_password == userData.new_password) {
  
   con.query(`select * from users where username = ?`, userData.username, (err, user) => {
     if (err) {
@@ -109,7 +145,7 @@ router.get('/changePassword', verifytoken, (req, res) => {
         bcrypt.compare(userData.old_password, user[0].password, (err, result)=> {
           if (err) throw err;
           if (!result) {
-            res.json({ error: 'Wrong old password' });
+            res.status(200).send({ message: 'Wrong old password' });
           } else {
             var password = '';
             bcrypt.hash(userData.new_password, saltRounds, (err, hash) => {
@@ -140,9 +176,9 @@ router.get('/changePassword', verifytoken, (req, res) => {
                      }
                      con.query("insert into expired_data SET ?", data, (err, result) => {
                        if (err) {
-                         res.send({ error: err.message });
+                         res.status(500).send({ error: err.message });
                        } else {
-                        res.json({ token: jwt.sign({ email: user[0].email, username: user[0].username, id: user[0].id }, 'RESTFULAPIs', { expiresIn: '1h' }),user });
+                        res.status(200).send({ message:'success',token: jwt.sign({ email: user[0].email, username: user[0].username, id: user[0].id }, 'RESTFULAPIs', { expiresIn: '1h' }),user });
                        }
                      });
 
@@ -154,10 +190,14 @@ router.get('/changePassword', verifytoken, (req, res) => {
           }
         });
       } else {
-        res.send('User not found');
+        res.status(200).send({message:'User not found'});
       }
     }
   });
+  
+  } else {
+	  res.status(200).send({message:"New password not match confirm password"});
+  }
 
 });
 
@@ -295,9 +335,9 @@ router.get('/deletePost', verifytoken, (req, res) => {
     } else {
       con.query("delete from answers WHERE post_id = ?",data.post_id, (err, result) => {
         if (err) {
-          res.send({ error: err.message });
+          res.status(500).send({ error: err.message });
         } else {
-          res.send('Post deleted...');
+          res.status(200).send({message:'Post deleted...'});
         }
       });
     }
@@ -512,6 +552,26 @@ router.get('/listLatestApprovedPosts', verifytoken, (req, res) => {
   });
 });
 
+router.get('/countApprovedPosts', verifytoken, (req, res) => {
+  con.query("select COUNT(*) As postCount from posts WHERE isApproved = 1 ",(err, result) => {
+    if (err) {
+      res.send({ error: err.message });
+    } else {
+      res.send({count:result[0].postCount});
+    }
+  });
+});
+
+router.get('/countRejectedPosts', verifytoken, (req, res) => {
+  con.query("select COUNT(*) As postCount from posts WHERE isRejected = 1 ",(err, result) => {
+    if (err) {
+      res.send({ error: err.message });
+    } else {
+      res.send({count:result[0].postCount});
+    }
+  });
+});
+
 router.get('/listMostAnsweredApprovedPosts', verifytoken, (req, res) => {
  
   var data=req.query.data;
@@ -527,7 +587,11 @@ router.get('/listMostAnsweredApprovedPosts', verifytoken, (req, res) => {
 });
 
 router.get('/listRejectedPosts', verifytoken, (req, res) => {
-  con.query("select * from posts WHERE isRejected = 1",(err, result) => {
+	
+  var data=req.query.data;
+  data=JSON.parse(data);
+  
+  con.query("select * from posts WHERE isRejected = 1 limit ?,?",[data.start,data.limit],(err, result) => {
     if (err) {
       res.send({ error: err.message });
     } else {
@@ -582,7 +646,7 @@ var searchString='';
     searchString+='|';
   });
 
-  con.query("SELECT DISTINCT posts.post_id, posts.title, posts.description FROM posts INNER JOIN answers ON posts.post_id = answers.post_id WHERE answers.description REGEXP  ? UNION SELECT posts.post_id, posts.title, posts.description FROM posts WHERE posts.title REGEXP  ? OR posts.description REGEXP  ? OR posts.solution REGEXP  ? LIMIT 0 , 30",[searchString,searchString,searchString,searchString],(err, result) => {
+  con.query("SELECT DISTINCT posts.post_id, posts.title, posts.description, posts.total_answers FROM posts INNER JOIN answers ON posts.post_id = answers.post_id WHERE answers.description REGEXP  ? UNION SELECT posts.post_id, posts.title, posts.description, posts.total_answers  FROM posts WHERE posts.title REGEXP  ? OR posts.description REGEXP  ? OR posts.solution REGEXP  ? LIMIT 0 , 30",[searchString,searchString,searchString,searchString],(err, result) => {
     if (err) {
       res.send({ error: err.message });
     } else {
@@ -594,9 +658,9 @@ var searchString='';
 router.get('/listUsers', verifytoken, (req, res) => {
   con.query("select user_id,username from users",(err, result) => {
     if (err) {
-      res.send({ error: err.message });
+      res.status(500).send({ error: err.message });
     } else {
-      res.send(result);
+      res.status(200).send(result);
     }
   });
 });
@@ -632,15 +696,52 @@ router.get('/removeUser', verifytoken, (req, res) => {
         } else {
           con.query("delete from answers WHERE user_id = ?",data.user_id,(err, result) => {
             if (err) {
-              res.send({ error: err.message });
+              res.status(500).send({ error: err.message });
             } else {
-              res.send("User removed successfully...");
+              res.status(200).send({message:"User removed successfully..."});
             }
           });
         }
       });
     }
   });
+});
+
+router.get('/forgotPassword', (req, res) => {
+  
+  var data=req.query.data;
+  data = JSON.parse(data);
+  
+  var userData = {
+    username:data.username
+  }
+  
+  con.query(`select * from users where username = ?`, userData.username, (err, user) => {
+    if (err) {
+      res.send({ error: err.message });
+    } else {
+      if (user.length > 0) {
+            var password = '';
+            bcrypt.hash(userData.new_password, saltRounds, (err, hash) => {
+              password = hash;
+              con.query("update users SET password = ?", password , (err, result) => {
+                if (err) {
+                  res.send({ error: err.message,message:"Password changing failed.." });
+                } else {
+
+                
+                }
+              });
+            });
+        
+      } else {
+        res.status(200).send({message:'User not found'});
+      }
+    }
+  });
+  
+ 
+
 });
 
 module.exports = router;
